070748c0d44f9b87bd47266883a280c8774d01bf
Michal Korniak <michael.korniak@gmail.com>
2023-12-29 16:41:25 +0100

    Add mimpirun implementation

--- mimpi.c
+++ mimpi.c
@@ -1,29 +1,71 @@
 /**
  * This file is for implementation of MIMPI library.
  * */
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
 
 #include "channel.h"
 #include "mimpi.h"
 #include "mimpi_common.h"
 
+
+static int world_size;
+static int world_rank;
+static int read_pipe_dsc;
+static int* write_pipe_dsc;
+
 void MIMPI_Init(bool enable_deadlock_detection) {
     channels_init();
 
-    TODO
+    world_size = atoi(getenv("MIMPI_WORLD_SIZE"));
+    world_rank = atoi(getenv("MIMPI_WORLD_RANK"));
+
+    read_pipe_dsc = 20 + 2 * world_rank;
+    write_pipe_dsc = malloc(sizeof(int) * world_size);
+
+    for (int i = 0; i < world_size; i++) {
+        write_pipe_dsc[i] = 21 + 2 * i;
+    }
+
+    //close reading pipes
+    for (int i = 20; i < 20 + 2 * world_size; i += 2) {
+        if (i != 20 + 2 * world_rank) {
+            ASSERT_SYS_OK(close(i));
+        }
+    }
+    //close this process writing pipe
+    ASSERT_SYS_OK(close(write_pipe_dsc[world_rank]));
+
+    for (int i = 20; i < 1024; i++) {
+        if (fcntl(i, F_GETFD) != -1) {
+            printf("File descriptor %d is open in process %d.\n", i, world_rank);
+        }
+    }
+
+    
 }
 
 void MIMPI_Finalize() {
-    TODO
-
+    
+    ASSERT_SYS_OK(close(read_pipe_dsc));
+    for (int i = 0; i < world_size; i++) {
+        if (i != world_rank) {
+            ASSERT_SYS_OK(close(write_pipe_dsc[i]));
+        }
+    }
+    free(write_pipe_dsc);
+    
     channels_finalize();
 }
 
 int MIMPI_World_size() {
-    TODO
+    return world_size;
 }
 
 int MIMPI_World_rank() {
-    TODO
+    return world_rank;
 }
 
 MIMPI_Retcode MIMPI_Send(
--- mimpirun.c
+++ mimpirun.c
@@ -2,8 +2,65 @@
  * This file is for implementation of mimpirun program.
  * */
 
-#include "mimpi_common.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
 
-int main() {
-    TODO
+#include "mimpi_common.h"
+#include "channel.h"
+
+int main(int argc, char* argv[]) {
+    if (argc < 3)
+       fatal("Usage: %s <number of processes> <program to run> [program arguments...]\n", argv[0]);
+    int const n = atoi(argv[1]);
+    const char* program_to_run = argv[2];
+    char** program_args = &argv[2];
+
+    ASSERT_SYS_OK(setenv("MIMPI_WORLD_SIZE", argv[1], 1));
+
+    int pipes[n][2];
+
+    for (int i = 0; i < n; i++) {
+        ASSERT_SYS_OK(channel(pipes[i]));
+        if (pipes[i][1] != 20 + 2 * i) {
+            ASSERT_SYS_OK(dup2(pipes[i][0], 20 + 2 * i));
+            if (pipes[i][0] != 20 + 2 * i)
+                ASSERT_SYS_OK(close(pipes[i][0]));
+            ASSERT_SYS_OK(dup2(pipes[i][1], 21 + 2 * i));
+            if (pipes[i][1] != 21 + 2 * i)
+                ASSERT_SYS_OK(close(pipes[i][1]));
+        }
+        else {
+            ASSERT_SYS_OK(dup2(pipes[i][1], 21 + 2 * i));
+            if (pipes[i][1] != 21 + 2 * i)
+                ASSERT_SYS_OK(close(pipes[i][1]));
+            ASSERT_SYS_OK(dup2(pipes[i][0], 20 + 2 * i));
+            if (pipes[i][0] != 20 + 2 * i)
+                ASSERT_SYS_OK(close(pipes[i][0]));
+        }
+    }
+    // Create n children
+    for (int i = 0; i < n; i++) {
+        pid_t pid = fork();
+        ASSERT_SYS_OK(pid);
+        if (pid == 0) {
+            char buf[4];
+            int ret = snprintf(buf, sizeof(buf), "%d", i);
+            if (ret < 0 || ret >= (int)sizeof(buf))
+                fatal("Error in snprintf.");
+            ASSERT_SYS_OK(setenv("MIMPI_WORLD_RANK", buf, 1));
+            ASSERT_SYS_OK(execvp(program_to_run, program_args));
+        }
+    }
+    // Close all pipes
+    for (int i = 20; i < 20 + 2 * n; i++) {
+        ASSERT_SYS_OK(close(i));
+    }
+    // Wait for all children
+    for (int i = 0; i < n; i++) {
+        ASSERT_SYS_OK(wait(NULL));
+    }
+    
+    return 0;
 }
